af4e7c52f4e9a7fc833f08cc65b03bb3e133d67f
inyourface34456 m <trenten_miller@etownstudents.org>
2024-03-19 15:46:07 +0000

    file writing test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Uncomment this block to pass the first stage
-use std::{fs::read_to_string, io::{Read, Write}, net::{TcpListener, TcpStream}};
+use std::{fs::{read_to_string, File}, io::{Read, Write}, net::{TcpListener, TcpStream}};
 use std::error::Error;
 use std::thread;
 use std::env::args;
@@ -38,15 +38,18 @@ fn gen_200_response_file<T: std::fmt::Display>(data: T, len: usize) -> String {
     format!("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}", len, data)
 }
 
-fn gen_404_response() -> String {
-    format!("HTTP/1.1 404 OK\r\n\r\n")
+fn gen_response(code: usize) -> String {
+    format!("HTTP/1.1 {} OK\r\n\r\n", code)
 }
 
 fn respond(stream: &mut TcpStream) -> Result<(), Box<dyn Error>> {
+    //let reader = BufReader::new(stream);
     let mut data = [0; 2048];              
     stream.read(&mut data)?;
     let data = String::from_utf8(data.to_vec())?.split("\r\n").map(|x| x.to_string()).collect::<Vec<String>>();
-    let path = data[0].split(' ').collect::<Vec<&str>>()[1];
+    let req_line = data[0].split(' ').collect::<Vec<&str>>();
+    let path = req_line[1];
+    let req_type = req_line[0];
     println!("{}", path);
 
     match path {
@@ -63,11 +66,17 @@ fn respond(stream: &mut TcpStream) -> Result<(), Box<dyn Error>> {
             } else if path.starts_with("/files") {
                 let dir = args().collect::<Vec<String>>()[2].to_string();
                 let path = path.split('/').collect::<Vec<&str>>()[2..].join("/");
-                println!("{}{}", dir, path);
-                if let Ok(to_send) = read_to_string(format!("{}{}", dir, path)) {
-                    stream.write_all(gen_200_response_file(&to_send, to_send.len()).as_bytes())?
-                } else {
-                    stream.write_all(gen_404_response().as_bytes())?
+                let path = format!("{}{}", dir, path);
+
+                if req_type == "GET" {
+                    if let Ok(to_send) = read_to_string(path) {
+                        stream.write_all(gen_200_response_file(&to_send, to_send.len()).as_bytes())?
+                    } else {
+                        stream.write_all(gen_response(404).as_bytes())?
+                    }
+                } else if req_type == "POST" {
+                    let to_write = &data[data.len()-1];
+                    File::create(path)?.write_all(to_write.as_bytes())?;
                 }
             } else {
                 stream.write_all(b"HTTP/1.1 404 NOT_FOUND\r\n\r\n")?

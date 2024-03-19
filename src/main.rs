9da20c2f3b8829345a7ca09f32c481b6088086e6
inyourface34456 m <trenten_miller@etownstudents.org>
2024-03-19 14:55:21 +0000

    test1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // Uncomment this block to pass the first stage
-use std::{io::{Read, Write}, net::TcpListener};
+use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
 use std::error::Error;
+use std::thread;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,30 +14,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                println!("accepted new connection");
-                let mut data: &mut [u8] = &mut [0; 2048];              
-                stream.read(&mut data)?;
-                let data = String::from_utf8(data.to_vec())?.split("\r\n").map(|x| x.to_string()).collect::<Vec<String>>();
-                let path = data[0].split(' ').collect::<Vec<&str>>()[1];
-                println!("{}", path);
-
-                match path {
-                    "/" => stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n")?,
-                    "/user-agent" => {
-                        let user_agent = &data[2].split(": ").collect::<Vec<&str>>()[1];
-                        stream.write_all(gen_200_response(&user_agent, user_agent.len()).as_bytes())?
-                    },
-                    _ => {
-                        if path.starts_with("/echo") {
-                            let to_echo: String = path.split('/').collect::<Vec<&str>>()[2..].join("/");
-                            let data = gen_200_response(&to_echo, to_echo.len());
-                            stream.write_all(data.as_bytes())?;
-                        } else {
-                            stream.write_all(b"HTTP/1.1 404 NOT_FOUND\r\n\r\n")?
-                        }
-                    }
-                    
-                }
+                thread::spawn(move || respond(&mut stream).expect("failed"));
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -48,4 +26,30 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 fn gen_200_response<T: std::fmt::Display>(data: T, len: usize) -> String {
     format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", len, data)
+}
+
+fn respond(stream: &mut TcpStream) -> Result<(), Box<dyn Error>> {
+    let mut data = vec![];              
+    stream.read_to_end(&mut data)?;
+    let data = String::from_utf8(data.to_vec())?.split("\r\n").map(|x| x.to_string()).collect::<Vec<String>>();
+    let path = data[0].split(' ').collect::<Vec<&str>>()[1];
+    println!("{}", path);
+
+    match path {
+        "/" => stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n")?,
+        "/user-agent" => {
+            let user_agent = &data[2].split(": ").collect::<Vec<&str>>()[1];
+            stream.write_all(gen_200_response(&user_agent, user_agent.len()).as_bytes())?
+        },
+        _ => {
+            if path.starts_with("/echo") {
+                let to_echo: String = path.split('/').collect::<Vec<&str>>()[2..].join("/");
+                let data = gen_200_response(&to_echo, to_echo.len());
+                stream.write_all(data.as_bytes())?;
+            } else {
+                stream.write_all(b"HTTP/1.1 404 NOT_FOUND\r\n\r\n")?
+            }
+        }
+    }
+    Ok(())
 }

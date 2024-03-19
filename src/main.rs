930b8d184a3355880233105a02933bb2a4de7a8c
inyourface34456 m <trenten_miller@etownstudents.org>
2024-03-19 14:20:18 +0000

    jgjfghjfg

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                     _ => {
                         if path.starts_with("/echo") {
                             let to_echo: String = path.split('/').collect::<Vec<&str>>()[2..].join("");
-                            let data = format!("HTTP/1.1 200 OK\r\n\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", to_echo.len(), to_echo);
+                            let data = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", to_echo.len(), to_echo);
                             stream.write_all(data.as_bytes())?;
                         } else {
                             stream.write_all(b"HTTP/1.1 404 NOT_FOUND\r\n\r\n")?
